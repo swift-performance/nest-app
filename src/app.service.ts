d0a3945589ca481f0f5237c5f3c76adc8ce8aff9
swift-sandeep <sandeep@swiftperformance.com>
2022-08-23 14:36:58 +1000

    fix-build

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-export const __VERSION__ = '0.0.2'
+export const __VERSION__ = 'testing'
 @Injectable()
 export class AppService {
   getVersion(): string {
