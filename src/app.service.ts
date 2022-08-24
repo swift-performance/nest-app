2f9c7ac1f7964df53d9b35c64708dc2e6f3f34a9
swift-sandeep <sandeep@swiftperformance.com>
2022-08-25 09:34:41 +1000

    test-app start

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-export const __VERSION__ = 'testing'
+export const __VERSION__ = '0.0.3'
 @Injectable()
 export class AppService {
   getVersion(): string {
